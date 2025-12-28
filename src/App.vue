c9210914e5908a6c529ae8a64c6377f664ea0e98
jufj <juranfuture@gmail.com>
2025-12-28 17:12:06 +0800

    feat: 模态框传送，通过原生模态控制开关

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import MyModal from "./components/MyModal.vue";
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+function toggleModalValue(value) {
+  isOpen.value = value;
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+    <MyModal :is-open="isOpen" @close="toggleModalValue">
+      <template #body>
+        <div class="custom-body">
+          This is some custom body content for the modal. 新加坡
+        </div>
+      </template>
+    </MyModal>
+    <button @click="isOpen = !isOpen">toggle modal status</button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/MyModal.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["close"]);
+const dialogRef = ref(null);
+
+const syncDialog = (open) => {
+  const dialog = dialogRef.value;
+  if (!dialog) return;
+  // open ? dialog.showModal() : dialog.close();
+  if (open) {
+    if (!dialog.open) dialog.showModal();
+  } else {
+    if (dialog.open) dialog.close();
+  }
+};
+
+watch(
+  () => props.isOpen,
+  (newVal) => {
+    syncDialog(newVal);
+  },
+  { immediate: true, flush: "post" }
+);
+
+const close = () => emit("close", false);
+</script>
+
+<template>
+  <Teleport to="#modal">
+    <dialog ref="dialogRef" @cancel.prevent="close" @close="close">
+      <slot name="header">
+        <div class="default-header">
+          <div class="title">Default Title</div>
+          <button class="close-button" @click="close">X</button>
+        </div>
+      </slot>
+      <slot name="body">
+        <div class="default-body">Default Body Content</div>
+      </slot>
+      <slot name="footer">
+        <div class="default-footer">
+          <button @click="close">Close</button>
+        </div>
+      </slot>
+    </dialog>
+  </Teleport>
+</template>
+
+<style scoped>
+dialog::backdrop {
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
